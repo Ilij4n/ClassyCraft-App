5382532c08b93f5da134707693e85e164cc7b8a6
Ilijan <ikojovic8123@raf.rs>
2023-12-03 16:57:06 +0100

    rad na kt2

---
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/painters/GeneralizacijaPainter.java
@@ -0,0 +1,9 @@
+package raf.dsw.classycraft.app.gui.swing.painters;
+
+import raf.dsw.classycraft.app.core.model.implementation.diagramElements.connections.Connection;
+
+public class GeneralizacijaPainter extends ConnectionPainter{
+    public GeneralizacijaPainter(Connection veza) {
+        super(veza);
+    }
+}
